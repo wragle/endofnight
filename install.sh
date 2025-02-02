4961eaca17a3ecb6173e7e294b78dd311dcf7e70
Anish Rai <anish1rai@yahoo.co.uk>
2025-02-02 11:51:02 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@ if [ ! -d "$HOME/.logs" ]; then
 	echo "Creating ~/.logs/endofnight directory"
 	mkdir $HOME/.logs/endofnight
 fi
-ln .vimrc $HOME/.config/endofnight/vimrc 2> /dev/null && echo "Created link for vimrc" || echo "Skipping vimrc, file already exists"
-ln endofnight $HOME/.local/bin/endofnight 2> /dev/null && echo "Created link for endofnight" || echo "Skipping endofnight, file already exists"
-ln log $HOME/.local/bin/log 2> /dev/null && echo "Created link for log" || echo "Skipping log, file already exists"
+
+ln .vimrc $HOME/.config/endofnight/vimrc 2> /dev/null && echo "Created link for vimrc" || echo "Skipping vimrc, link already exists"
+ln endofnight $HOME/.local/bin/endofnight 2> /dev/null && echo "Created link for endofnight" || echo "Skipping endofnight, link already exists"
+ln log $HOME/.local/bin/log 2> /dev/null && echo "Created link for log" || echo "Skipping log, link already exists"
+ln log-calandar $HOME/.local/bin/log-calandar 2> /dev/null && echo "Created link for log-calandar" || echo "Skipping log-calandar, link already exists"
+ln log-search $HOME/.local/bin/log-search 2> /dev/null && echo "Created link for log-search" || echo "Skipping log-search, link already exists"
 
 echo "Installation complete"
