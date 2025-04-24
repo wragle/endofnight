bd60e41ad6f33a1255c1c836989c9f43430fa5ea
Anish Rai <anish1rai@yahoo.co.uk>
2025-04-24 19:48:12 +0100

    changed ln to cp in install script

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ if [ ! -d "$HOME/.local/share/log" ]; then
 	mkdir $HOME/.local/share/log/endofnight
 fi
 
-ln .vimrc $HOME/.config/endofnight/vimrc 2> /dev/null && echo "Created link for vimrc" || echo "Skipping vimrc, link already exists"
-ln endofnight $HOME/.local/bin/endofnight 2> /dev/null && echo "Created link for endofnight" || echo "Skipping endofnight, link already exists"
-ln log $HOME/.local/bin/log 2> /dev/null && echo "Created link for log" || echo "Skipping log, link already exists"
-ln log-calandar $HOME/.local/bin/log-calandar 2> /dev/null && echo "Created link for log-calandar" || echo "Skipping log-calandar, link already exists"
-ln log-search $HOME/.local/bin/log-search 2> /dev/null && echo "Created link for log-search" || echo "Skipping log-search, link already exists"
+cp vimrc $HOME/.config/endofnight/vimrc 2> /dev/null && echo "Copied vimrc"
+cp endofnight $HOME/.local/bin/endofnight 2> /dev/null && echo "Copied endofnight"
+cp log $HOME/.local/bin/log 2> /dev/null && echo "Copied log"
+cp log-calandar $HOME/.local/bin/log-calandar 2> /dev/null && echo "Copied log-calandar"
+cp log-search $HOME/.local/bin/log-search 2> /dev/null && echo "Copied log-search"
 
 echo "Installation complete"
